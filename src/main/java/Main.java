1f120ace6827b78d101b7b506a9660002fdd34f8
Danaya Dews <danayadews@gmail.com>
2021-11-13 16:38:05 -0500

    print first project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("My name is Danaya");
+        System.out.println("This is my first project.");
     }
 }
